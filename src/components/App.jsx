ae2be76322ff61b671fb8d7081515a93b9090612
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-09-19 21:54:26 +0800

    style: Finish the UI design
    - Create class component.
    - Finish UI cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,20 +10,30 @@ function App() {
   }, []);
 
   return (
-    <>
-      <h1>POKEMON</h1>
+    <section className="container-xl">
+      <div className="flex items-center justify-between mt-10">
+        <h1 className="mt-4 text-4xl font-bold text-center text-amber-400">Pokemon Memory Game</h1>
+        <p className="text-lg text-amber-400">
+          Score:
+          <br />
+          Best Score:
+        </p>
+      </div>
+      <br />
 
-      {
-        !loading && pokemonList.map((data) => {
-          return (
-            <div key={data.id}>
-              <img src={data.image} alt={`${data.name} image`} />
-              <p>{data.name}</p>
-            </div>
-          )
-        })
-      }
-    </>
+      <div className="grid grid-cols-1 gap-4 mt-6 sm:grid-cols-4">
+        {
+          !loading && pokemonList.map((data) => {
+            return (
+              <div key={data.id} className="grid gap-2 p-4 transition duration-300 ease-in-out rounded-lg cursor-pointer hover:bg-white/50 hover:scale-95 bg-white/10 backdrop-blur-md place-content-center">
+                <img className="w-50 h-50" src={data.image} alt={`${data.name} image`} />
+                <p className="font-serif text-2xl italic font-bold text-center uppercase text-amber-400">{data.name}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
