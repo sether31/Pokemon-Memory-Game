a436c70d2f9e2c06ac93802fb1953ca4683481a3
Seth Hernandez <sethmichaelhernandez@gmail.com>
2025-09-19 23:35:23 +0800

    feat(core): Add main functionality like shuffle, scoreboard and interactive click.
    - Create utility shuffle.
    - Create scoreboard for best score and current score.
    - Create handleSelect for clicking events.

---
+++ src/utils/shuffle.jsx
@@ -0,0 +1,12 @@
+function Shuffle(data) {
+  const shuffled = [...data]
+  for (let i = data.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+
+  return shuffled;
+}
+
+export default Shuffle;
